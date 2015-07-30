fdf28c12a1e9ac61e9d69e213f005bbffb1923ef
baraayyash <baraayyash93@gmail.com>
2015-07-30 16:20:29 +0300

    starting

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ang1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ang1App
+ */
+angular.module('ang1App')
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.deleteshit = function(id){console.log(id);};
+
+    $scope.awesomeThings = [
+      {"id" : "0", "title" : "HTML5 Boilerplate"},
+     {"id" : "1", "title" : "HTML5 Boilerplate1"},
+     {"id" : "2", "title" : "HTML5 Boilerplate2"}
+    ];
+  });
