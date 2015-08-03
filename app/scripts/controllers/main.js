013dccf8f20dd1098cdc3aa4dc895363cd1eebd1
Waleed <walid.bzu@gmail.com>
2015-08-03 10:39:25 +0300

    modying layout -2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,19 +10,19 @@
 angular.module('ang1App')
   .controller('MainCtrl', function ($scope) {
 
-  	jQuery(document).ready(function() {
-   Metronic.init(); // init metronic core componets
-   Layout.init(); // init layout
-   QuickSidebar.init(); // init quick sidebar
-Demo.init(); // init demo features
-   Index.init();
-   Index.initDashboardDaterange()
-   Index.initCalendar(); // init index page's custom scripts
-   Index.initCharts(); // init index page's custom scripts
-   Index.initChat();
-   Index.initMiniCharts();
-   Tasks.initDashboardWidget();
-});
+  jQuery(document).ready(function() {
+    Metronic.init(); // init metronic core componets
+    Layout.init(); // init layout
+    QuickSidebar.init(); // init quick sidebar
+    Demo.init(); // init demo features
+    Index.init();
+    Index.initDashboardDaterange()
+    Index.initCalendar(); // init index page's custom scripts
+    Index.initCharts(); // init index page's custom scripts
+    Index.initChat();
+    Index.initMiniCharts();
+    Tasks.initDashboardWidget();
+  });
 
     $scope.deleteshit = function(id){console.log(id);};
 
@@ -31,4 +31,5 @@ Demo.init(); // init demo features
      {"id" : "1", "title" : "HTML5 Boilerplate1"},
      {"id" : "2", "title" : "HTML5 Boilerplate2"}
     ];
+
   });
